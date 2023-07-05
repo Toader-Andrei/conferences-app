01b0728b0a070ddc3954d96d539d6d2990ef80a7
Pycul <toaderandrei58@gmail.com>
2023-07-05 19:58:55 +0300

    speakers page + schedule page + fixing code

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,11 +1,3 @@
-fetch("http://localhost:3000/speakers")
-  .then((response) => response.json())
-  .then((user) => {
-    user.forEach((speaker) => {
-      createSpeakerCard(speaker);
-    });
-  });
-
 // When the user scrolls the page, execute myFunction
 window.onscroll = function () {
   myFunction();
@@ -53,19 +45,19 @@ window.addEventListener("DOMContentLoaded", (e) => {
     .then((response) => response.json())
     .then((speech) => {
       speech.forEach((event) => {
-        createSpeechCard(event);
+        createSpeechesCard(event);
       });
     });
   const monday = document.querySelector(".right-container");
   monday.addEventListener("click", (event) => {
     const speeches = document.querySelector(".speeches-container");
-    speeches.innerHTML = "";
+    speeches.innerText = "";
     event.preventDefault();
     fetch("http://localhost:3000/speeches?date=" + "28 July 2023")
       .then((response) => response.json())
       .then((speech) => {
         speech.forEach((event) => {
-          createSpeechCard(event);
+          createSpeechesCard(event);
         });
       });
   });
@@ -73,12 +65,12 @@ window.addEventListener("DOMContentLoaded", (e) => {
 const tueday = document.querySelector(".middle-container");
 tueday.addEventListener("click", () => {
   const speeches = document.querySelector(".speeches-container");
-  speeches.innerHTML = "";
+  speeches.innerText = "";
   fetch("http://localhost:3000/speeches?date=" + "29 July 2023")
     .then((response) => response.json())
     .then((speech) => {
       speech.forEach((event) => {
-        createSpeechCard(event);
+        createSpeechesCard(event);
       });
     });
 });
@@ -86,18 +78,18 @@ tueday.addEventListener("click", () => {
 const wednesday = document.querySelector(".left-container");
 wednesday.addEventListener("click", () => {
   const speeches = document.querySelector(".speeches-container");
-  speeches.innerHTML = "";
+  speeches.innerText = "";
   fetch("http://localhost:3000/speeches?date=" + "30 July 2023")
     .then((response) => response.json())
     .then((speech) => {
       speech.forEach((event) => {
-        createSpeechCard(event);
+        createSpeechesCard(event);
       });
     });
 });
 
 // adding speeches
-function createSpeechCard(speech) {
+function createSpeechesCard(speech) {
   const scheduleContainer = document.querySelector(".speeches-container");
 
   const speechContainer = document.createElement("div");
@@ -177,19 +169,19 @@ function createSpeechCard(speech) {
     "class",
     "conference-btn col-sm-4 col-lg-4 col-xl-2 mt-2 mt-md-1 mt-lg-0"
   );
-  // collapse
+
   const speechBtn = document.createElement("button");
   speechBtn.setAttribute("class", "btn btn-primary collapsed");
   speechBtn.setAttribute("type", "button");
   speechBtn.setAttribute("data-bs-toggle", "collapse");
-  speechBtn.setAttribute("data-bs-target", "#collapseExample-" + speech.id);
+  speechBtn.setAttribute("data-bs-target", "#collapseExample");
   speechBtn.setAttribute("aria-expended", "false");
   speechBtn.setAttribute("aria-controls", "collapseExample");
   speechBtn.innerText = "Participate";
 
   const formContainer = document.createElement("div");
   formContainer.setAttribute("class", "mt-3 collapse");
-  formContainer.setAttribute("id", "collapseExample-" + speech.id);
+  formContainer.setAttribute("id", "collapseExample");
 
   const form = document.createElement("div");
   form.setAttribute("class", "row g-3 form card card-body");
@@ -253,107 +245,4 @@ function createSpeechCard(speech) {
   speechContainer.appendChild(speechCard);
 
   scheduleContainer.appendChild(speechContainer);
-}
-
-//adding speaker's id on image
-function createSpeakerCard(speaker) {
-  const speakersRow = document.querySelector(".speakers-row");
-
-  const speakerContainer = document.createElement("div");
-  speakerContainer.setAttribute(
-    "class",
-    "col-12 col-md-4 mb-4 mt-sm-4 mt-md-0"
-  );
-
-  const speakerImageDescriptionContainer = document.createElement("div");
-  speakerImageDescriptionContainer.setAttribute(
-    "class",
-    "speaker-image-container"
-  );
-  speakerImageDescriptionContainer.setAttribute("speaker-id", speaker.id);
-
-  const speakerLink = document.createElement("a");
-  speakerLink.setAttribute("href", "speaker.html");
-
-  const speakerImage = document.createElement("img");
-  speakerImage.setAttribute("class", "speakers-imgs cursor-pointer");
-  speakerImage.setAttribute("src", speaker.image);
-  speakerImage.setAttribute("alt", speaker.category);
-  speakerImage.addEventListener("click", () => {
-    localStorage.setItem("speakerId", JSON.stringify(speaker.id));
-  });
-
-  speakerLink.appendChild(speakerImage);
-
-  speakerImageDescriptionContainer.appendChild(speakerLink);
-
-  const speakerNameCategoryContainer = document.createElement("div");
-  speakerNameCategoryContainer.setAttribute("class", "speaker-description");
-
-  speakerImageDescriptionContainer.appendChild(speakerNameCategoryContainer);
-
-  const speakerName = document.createElement("p");
-  speakerName.setAttribute("class", "text-uppercase");
-  speakerName.innerText = speaker.name;
-
-  const speakerCategory = document.createElement("p");
-  speakerCategory.setAttribute("class", "text-uppercase text-pink");
-  speakerCategory.innerText = speaker.category;
-
-  speakerNameCategoryContainer.appendChild(speakerName);
-  speakerNameCategoryContainer.appendChild(speakerCategory);
-
-  speakerContainer.appendChild(speakerImageDescriptionContainer);
-
-  speakersRow.appendChild(speakerContainer);
-}
-function createForm(e) {
-  const speechRow = document.querySelector(".card-row");
-
-  const formContainer = document.createElement("div");
-  formContainer.setAttribute("class", "mt-3 collapse");
-  formContainer.setAttribute("id", "collapseExample");
-
-  const form = document.createElement("div");
-  form.setAttribute("class", "row g-3 form card card-body");
-
-  const formFirstNameContainer = document.createElement("div");
-  formFirstNameContainer.setAttribute("class", "col-md-6");
-
-  const firstNameInput = document.createElement("input");
-  firstNameInput.setAttribute("type", "text");
-  firstNameInput.setAttribute("class", "form-control");
-  firstNameInput.setAttribute("placeholder", "First name");
-  firstNameInput.setAttribute("aria-label", "First name");
-
-  formFirstNameContainer.appendChild(firstNameInput);
-
-  const formLastNameContainer = document.createElement("div");
-  formLastNameContainer.setAttribute("class", "col-md-6");
-
-  const firstLastNameInput = document.createElement("input");
-  firstLastNameInput.setAttribute("type", "text");
-  firstLastNameInput.setAttribute("class", "form-control");
-  firstLastNameInput.setAttribute("placeholder", "Last name");
-  firstLastNameInput.setAttribute("aria-label", "Last name");
-
-  formLastNameContainer.appendChild(firstLastNameInput);
-
-  const submitBtnContainer = document.createElement("div");
-  submitBtnContainer.setAttribute("class", "col-12 mb-3");
-
-  const submitBtn = document.createElement("button");
-  submitBtn.innerText = "Submit";
-  submitBtn.setAttribute("type", "button");
-  submitBtn.setAttribute("class", "btn btn-primary");
-
-  submitBtnContainer.appendChild(submitBtn);
-
-  form.appendChild(formFirstNameContainer);
-  form.appendChild(formLastNameContainer);
-  form.appendChild(submitBtnContainer);
-
-  formContainer.appendChild(form);
-
-  speechRow.appendChild(formContainer);
 }
--- assets/js/speaker.js
+++ assets/js/speaker.js
@@ -1,11 +1,15 @@
 const speakerId = JSON.parse(localStorage.getItem("speakerId"));
-console.log(speakerId);
 
 fetch("http://localhost:3000/speakers/" + speakerId)
   .then((response) => response.json())
   .then((speaker) => {
     createSpeakerPage(speaker);
   });
+
+function clearForm() {
+  const form = document.querySelector(".form");
+  form.reset();
+}
 
 function createSpeakerPage(speaker) {
   const speakerName = document.querySelector(".speaker-name");
@@ -16,4 +20,282 @@ function createSpeakerPage(speaker) {
 
   const speakerImage = document.querySelector(".speaker-image");
   speakerImage.setAttribute("src", speaker.image);
+
+  const authorContainer = document.querySelector(".post-author-avatar");
+
+  const authorAvatar = document.createElement("img");
+  authorAvatar.setAttribute("src", speaker.image);
+  authorAvatar.setAttribute("class", "avatar");
+
+  authorContainer.appendChild(authorAvatar);
+
+  const authorContentContainer = document.querySelector(".post-author-content");
+
+  const authorContentHeading = document.createElement("h5");
+  authorContentHeading.setAttribute("class", "mt-2");
+  authorContentHeading.innerText = speaker.name;
+
+  const authorContentCategory = document.createElement("h5");
+  authorContentCategory.setAttribute("class", "text-pink");
+  authorContentCategory.innerText = speaker.category;
+
+  const authorContentMindSet = document.createElement("h6");
+  authorContentMindSet.setAttribute("class", "my-2 text-secondary");
+  authorContentMindSet.innerText = speaker.mindSet;
+
+  authorContentContainer.appendChild(authorContentHeading);
+  authorContentContainer.appendChild(authorContentCategory);
+  authorContentContainer.appendChild(authorContentMindSet);
+
+  const authorContentSocialContainer = document.querySelectorAll(
+    ".post-author-social-media"
+  );
+  console.log(authorContentSocialContainer);
+  const authorContentFacebook = document.createElement("a");
+  authorContentFacebook.setAttribute("href", "#");
+  authorContentFacebook.setAttribute("class", "p-2");
+
+  const facebookIcon = document.createElement("i");
+  facebookIcon.setAttribute("class", "fa-brands fa-facebook");
+
+  authorContentFacebook.appendChild(facebookIcon);
+
+  const authorContentInstagram = document.createElement("a");
+  authorContentInstagram.setAttribute("href", "#");
+  authorContentInstagram.setAttribute("class", "p-2");
+
+  const instagramIcon = document.createElement("i");
+  instagramIcon.setAttribute("class", "fa-brands fa-instagram");
+
+  authorContentInstagram.appendChild(instagramIcon);
+
+  const authorContentTikTok = document.createElement("a");
+  authorContentTikTok.setAttribute("href", "#");
+  authorContentTikTok.setAttribute("class", "p-2");
+
+  const tiktokIcon = document.createElement("i");
+  tiktokIcon.setAttribute("class", "fa-brands fa-tiktok");
+
+  authorContentTikTok.appendChild(tiktokIcon);
+
+  const authorContentTwitter = document.createElement("a");
+  authorContentTwitter.setAttribute("href", "#");
+  authorContentTwitter.setAttribute("class", "p-2");
+
+  const twitterIcon = document.createElement("i");
+  twitterIcon.setAttribute("class", "fa-brands fa-twitter");
+
+  authorContentTwitter.appendChild(twitterIcon);
+
+  authorContentSocialContainer.forEach((e) => {
+    e.appendChild(authorContentFacebook);
+    e.appendChild(authorContentInstagram);
+    e.appendChild(authorContentTikTok);
+    e.appendChild(authorContentTwitter);
+  });
+
+  const title = document.querySelector(".title");
+  title.innerText = speaker.title;
+
+  const date = document.querySelector(".date");
+  date.innerText = speaker.date;
+
+  const name = document.querySelector(".name");
+  name.innerText = speaker.name;
+
+  const location = document.querySelector(".location");
+  location.innerText = speaker.location;
+
+  fetch("http://localhost:3000/comments?postId=" + speakerId)
+    .then((response) => response.json())
+    .then((comments) => {
+      console.log();
+      comments.forEach((comment) => {
+        createComment(comment);
+      });
+      const commentsContainer = document.querySelector(".comments");
+
+      const commentCounter = document.querySelector(".comments-counter");
+      console.log(commentCounter, comments);
+
+      commentCounter.innerText = comments.length || 0;
+
+      if (commentsContainer.length === 0) {
+        const emptyCommentsContainer = document.createElement("div");
+        emptyCommentsContainer.classList.add("no-comments-warning");
+
+        const emptyMessage = document.createElement("p");
+        emptyMessage.classList.add("text-center");
+        emptyMessage.innerText = "No comments!";
+
+        emptyCommentsContainer.appendChild(emptyMessage);
+        commentSection.appendChild(emptyCommentsContainer);
+      }
+    });
 }
+
+function clearForm() {
+  const form = document.querySelector(".form");
+  form.reset();
+}
+
+function createComment(comment) {
+  const commentSection = document.querySelector(".comments");
+
+  const commentContainer = document.createElement("div");
+  commentContainer.setAttribute(
+    "class",
+    "d-flex aling-content-center m-4 border-radius p-2 bg-light shadow comment-container"
+  );
+  commentContainer.setAttribute("data-id", comment.id);
+
+  const profile = document.createElement("div");
+  profile.setAttribute(
+    "class",
+    "profile-img-name text-center m-2 max-witdh-150"
+  );
+
+  const commentImage = document.createElement("img");
+  commentImage.setAttribute(
+    "src",
+    "https://img.freepik.com/premium-photo/gorgeous-female-brunette-model-white-clothes_149155-2130.jpg"
+  );
+  commentImage.setAttribute("class", "comment-image");
+
+  const commentTextContainer = document.createElement("div");
+  commentTextContainer.setAttribute(
+    "class",
+    "d-flex align-items-center flex-grow-1 m-2"
+  );
+
+  const commentNameContainer = document.createElement("div");
+  commentNameContainer.setAttribute("class", "actualComment");
+
+  const commentName = document.createElement("h5");
+  commentName.classList.add("m-0");
+  commentName.innerText = comment.firstname + " " + comment.lastname;
+
+  profile.appendChild(commentImage);
+  profile.appendChild(commentName);
+
+  const commentDescription = document.createElement("p");
+  commentDescription.setAttribute("class", "fs-5");
+  commentDescription.innerText = comment.comment;
+
+  commentNameContainer.appendChild(commentDescription);
+
+  const commentDate = document.createElement("p");
+  commentDate.setAttribute("class", "text-secondary");
+  commentDate.innerText = comment.time;
+
+  commentNameContainer.appendChild(commentDate);
+
+  const iconsContainer = document.createElement("div");
+  iconsContainer.setAttribute("class", "align-self-center text-center");
+
+  const clearBtn = document.createElement("i");
+  clearBtn.setAttribute(
+    "class",
+    "fa-solid fa-trash align-self-center m-4 fs-4"
+  );
+
+  clearBtn.setAttribute("data-id", comment.id);
+
+  clearBtn.addEventListener("click", () => {
+    const commentId = document.querySelector(
+      '.fa-trash[data-id="' + comment.id + '"]'
+    );
+
+    fetch("http://localhost:3000/comments/" + comment.id, {
+      method: "DELETE",
+    }).then(() => {
+      commentContainer.remove(commentId);
+      const comments = document.querySelectorAll(".comment-container");
+
+      if (comments.length === 0) {
+        const emptyCommentsContainer = document.createElement("div");
+        emptyCommentsContainer.classList.add("no-comments-warning");
+
+        const emptyMessage = document.createElement("p");
+        emptyMessage.classList.add("text-center");
+        emptyMessage.innerText = "No comments!";
+
+        emptyCommentsContainer.appendChild(emptyMessage);
+        commentSection.appendChild(emptyCommentsContainer);
+      }
+      document.querySelector(".comments-counter").innerText =
+        +document.querySelector(".comments-counter").innerText - 1;
+    });
+  });
+
+  commentContainer.appendChild(profile);
+  commentTextContainer.appendChild(commentNameContainer);
+  commentContainer.appendChild(commentTextContainer);
+
+  iconsContainer.appendChild(clearBtn);
+
+  commentContainer.appendChild(iconsContainer);
+
+  commentSection.appendChild(commentContainer);
+}
+
+const commentsContainer = document.querySelector(".comments");
+console.log(commentsContainer);
+
+const submitBtn = document.querySelector(".submit");
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const firstName = document.querySelector(".firstName-input").value;
+  const lastName = document.querySelector(".lastName-input").value;
+  const comment = document.querySelector(".comments-input").value;
+  const date = new Date().toLocaleDateString("ro-RO");
+
+  const validation = document.createElement("p");
+  validation.classList.add("text-danger", "fs-5", "validation-message");
+  validation.innerText =
+    "Do you want to remain anonymous? then leave this site, here is no place for people like you! Try Tor Browser :)";
+
+  const validationsLocation = document.querySelector(".validations");
+
+  if (firstName === "" || lastName === "" || comment === "") {
+    validationsLocation.appendChild(validation);
+    if (validation) {
+      const validationMessage = document.querySelector(".validation-message");
+      validationMessage.remove();
+      validationsLocation.appendChild(validation);
+    }
+  } else {
+    const validationMessage = document.querySelector(".validation-message");
+    if (validationMessage) {
+      validationMessage.remove();
+    }
+    fetch("http://localhost:3000/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        firstname: firstName,
+        lastname: lastName,
+        comment: comment,
+        postId: speakerId,
+        time: date,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((comment) => {
+        const warningMessageContainer = document.querySelector(
+          ".no-comments-warning"
+        );
+        if (warningMessageContainer) {
+          warningMessageContainer.remove();
+        }
+        createComment(comment);
+        document.querySelector(".comments-counter").innerText =
+          +document.querySelector(".comments-counter").innerText + 1;
+      });
+  }
+
+  clearForm();
+});
